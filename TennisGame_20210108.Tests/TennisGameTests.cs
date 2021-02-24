f9b9048a3612117f4a84a483b622eed0d10da6f4
art <partypeopleland@gmail.com>
2021-02-24 15:47:07 +0800

    init

---
+++ TennisGame_20210108.Tests/TennisGameTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGame_20210108.Tests
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        readonly TennisGame _tennisGame = new TennisGame("Player1", "Player2");
+
+        [TestMethod]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            _tennisGame.SetPlayer1Score(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            _tennisGame.SetPlayer1Score(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [TestMethod]
+        public void Forty_Love()
+        {
+            _tennisGame.SetPlayer1Score(3);
+            ScoreShouldBe("Forty Love");
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            _tennisGame.SetPlayer2Score(1);
+            ScoreShouldBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            _tennisGame.SetPlayer2Score(2);
+            ScoreShouldBe("Love Thirty");
+        }
+
+        [TestMethod]
+        public void Love_Forty()
+        {
+            _tennisGame.SetPlayer2Score(3);
+            ScoreShouldBe("Love Forty");
+        }
+
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            _tennisGame.SetPlayer1Score(1);
+            _tennisGame.SetPlayer2Score(1);
+            ScoreShouldBe("Fifteen All");
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            _tennisGame.SetPlayer1Score(2);
+            _tennisGame.SetPlayer2Score(2);
+            ScoreShouldBe("Thirty All");
+        }
+
+        [TestMethod]
+        public void Deuce()
+        {
+            _tennisGame.SetPlayer1Score(3);
+            _tennisGame.SetPlayer2Score(3);
+            ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Deuce_When_4_4()
+        {
+            _tennisGame.SetPlayer1Score(4);
+            _tennisGame.SetPlayer2Score(4);
+            ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Player1_Adv()
+        {
+            _tennisGame.SetPlayer1Score(4);
+            _tennisGame.SetPlayer2Score(3);
+            ScoreShouldBe("Player1 Adv");
+        }
+        
+        [TestMethod]
+        public void Player2_Adv()
+        {
+            _tennisGame.SetPlayer1Score(3);
+            _tennisGame.SetPlayer2Score(4);
+            ScoreShouldBe("Player2 Adv");
+        }
+        
+        [TestMethod]
+        public void Player1_Win()
+        {
+            _tennisGame.SetPlayer1Score(5);
+            _tennisGame.SetPlayer2Score(3);
+            ScoreShouldBe("Player1 Win");
+        }
+                                                                                                                          
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennisGame.Score());
+        }
+    }
+}
---
+++ TennisGame_20210108/TennisGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisGame_20210108
+{
+    public class TennisGame
+    {
+        // 46 min
+        private int _player1Score;
+        private int _player2Score;
+
+        private readonly string _player2Name;
+        private readonly string _player1Name;
+
+
+        private readonly Dictionary<int, string> _dict = new Dictionary<int, string>()
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"}
+        };
+
+        public TennisGame(string player1Name, string player2Name)
+        {
+            this._player1Name = player1Name;
+            this._player2Name = player2Name;
+        }
+
+
+        public string Score()
+        {
+            return IsSameScore()
+                ? IsSameScoreAndIsDeuce() ? ResultDeuce() : ResultSameScore()
+                : IsReadyForWin()
+                    ? ResultAdvOrWin()
+                    : ResultNormalScore();
+        }
+
+        private string ResultAdvOrWin()
+        {
+            return $"{AdvPlayer()} {(IsAdv()? "Adv":"Win")}";
+        }
+
+        private string ResultNormalScore()
+        {
+            return $"{_dict[_player1Score]} {_dict[_player2Score]}";
+        }
+
+        private bool IsAdv()
+        {
+            return Math.Abs(_player1Score - _player2Score) == 1;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return _player1Score > 3 || _player2Score > 3;
+        }
+
+        private string ResultSameScore()
+        {
+            return $"{_dict[_player1Score]} All";
+        }
+
+        private static string ResultDeuce()
+        {
+            return "Deuce";
+        }
+
+        private bool IsSameScoreAndIsDeuce()
+        {
+            return IsSameScore() && _player1Score >= 3;
+        }
+
+        private string AdvPlayer()
+        {
+            return _player1Score > _player2Score ? _player1Name : _player2Name;
+        }
+
+        private bool IsSameScore()
+        {
+            return _player1Score == _player2Score;
+        }
+
+        public void SetPlayer1Score(int score)
+        {
+            this._player1Score = score;
+        }
+
+        public void SetPlayer2Score(int score)
+        {
+            this._player2Score = score;
+        }
+    }
+}
